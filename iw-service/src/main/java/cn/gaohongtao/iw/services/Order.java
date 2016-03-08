f8156dd16fbd7e62b13e62af6b37aac49ecd2ae8
gaoht <gaoht@asiainfo.com>
2016-03-09 00:15:54 +0800

    改造订单

--- iw-service/src/main/java/cn/gaohongtao/iw/services/Order.java
+++ iw-service/src/main/java/cn/gaohongtao/iw/services/Order.java
@@ -24,6 +24,7 @@ import cn.gaohongtao.iw.protocol.iw.OrderPlaceRequest;
 import cn.gaohongtao.iw.protocol.iw.OrderPlaceResponse;
 import cn.gaohongtao.iw.protocol.wechat.NotifyPayRequest;
 import cn.gaohongtao.iw.protocol.wechat.NotifyPayResponse;
+import com.google.common.collect.Interner;
 import com.google.common.collect.Lists;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
@@ -85,10 +86,37 @@ public class Order {
 //            throw new ServiceException(unifiedOrderResponse.getReturn_msg());
 //        }
 
+        MongoCollection<Document> userCollection = MongoUtil.getDatabase("basic").getCollection("user");
+        Document user = userCollection.find(eq("_id", request.getUserId()))
+                .first();
+        boolean updateUserPacket = false;
+        if (request.getItem().containsKey("packet")) {
+            if (null == user || !user.containsKey("packet")) {
+                throw new ServiceException("该用户没有领取红包");
+            } else if (!((Document) user.get("packet")).getString("packetId").equals(request.getItem().get("packet"))) {
+                throw new ServiceException("该红包5位码不是该用户的");
+            } else if (!((Document) user.get("packet")).getString("state").equals("new")) {
+                throw new ServiceException("用户已经使用过红包");
+            } else {
+                updateUserPacket = true;
+            }
+        }
         Document prodDoc = MongoUtil.getDatabase("product").getCollection("items").find(eq("_id", new ObjectId(request.getItem().get("itemId")))).first();
         if (prodDoc == null) {
             throw new ServiceException("不存在下单的商品");
         }
+        int itemNum = Integer.valueOf(request.getItem().get("itemNum"));
+        int sizeNumber = 0;
+        for (Document productDoc : (List<Document>) prodDoc.get("sizeList")) {
+            if (productDoc.getString("size").equals(request.getItem().get("size"))) {
+                sizeNumber = Integer.valueOf(productDoc.get("count").toString());
+            }
+        }
+        if (sizeNumber < itemNum) {
+            throw new ServiceException("该号码库存不足");
+        }
+
+
         request.getItem().put("name", prodDoc.getString("name"));
         request.getItem().put("color", ((List) prodDoc.get("color")).get(0).toString());
 
@@ -104,25 +132,18 @@ public class Order {
         orderDoc.append("msg", "已经成功下单，等待支付结果");
         MongoUtil.getDatabase("order").getCollection("items").insertOne(orderDoc);
 
-        MongoCollection<Document> col = MongoUtil.getDatabase("basic").getCollection("user");
-        Document user = col.find(eq("_id", request.getUserId()))
-                .first();
         if (user == null) {
-            col.insertOne(new Document("_id", request.getUserId()).append("address", Lists.newArrayList(request.getAddressInfo())));
+            userCollection.insertOne(new Document("_id", request.getUserId()).append("address", Lists.newArrayList(request.getAddressInfo())));
         } else {
-            col.updateOne(new Document("_id", request.getUserId()), new Document("$addToSet", new Document("address", request.getAddressInfo())));
-        }
-        int sizeNumber = 0;
-        for (Document productDoc : (List<Document>) prodDoc.get("sizeList")) {
-            if (productDoc.getString("size").equals(request.getItem().get("size"))) {
-                sizeNumber = Integer.valueOf(productDoc.getString("count"));
+            Document updateDoc = new Document("$addToSet", new Document("address", request.getAddressInfo()));
+            if (updateUserPacket) {
+                updateDoc.append("$set", new Document("packet.state", "used").append("packet.state_date", now));
             }
+            userCollection.updateOne(new Document("_id", request.getUserId()), updateDoc);
         }
-        if (sizeNumber < 1) {
-            throw new ServiceException("该号码已经售完");
-        }
+
         MongoUtil.getDatabase("product").getCollection("items").updateOne(new Document("_id", new ObjectId(request.getItem().get("itemId"))).append("sizeList.size", request.getItem().get("size")),
-                new Document("$set", new Document("sizeList.$.count", --sizeNumber)));
+                new Document("$set", new Document("sizeList.$.count", String.valueOf(sizeNumber - itemNum))));
 
         response.setOrderId(orderId);
         response.setPrepayId("-1");
